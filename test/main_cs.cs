9d17d3ddeb88add1f534247ee0c6b07429d7d755
ChristianCella <christian.cella@polimi.it>
2025-04-22 08:50:51 +0200

    100 PSO iterations for the random case. Still no possibility to obtain the same results as the PSO

--- test/main_cs.cs
+++ test/main_cs.cs
@@ -27,8 +27,8 @@ class Program
     static int collision_flag = 0;
 
     // socket variables
-    static string ip_address = "127.0.0.41";
-    static int port = 141;
+    static string ip_address = "127.0.0.42";
+    static int port = 142;
 
     // Static variables to create the robot program
     static string type_of_motion = "PTP";
